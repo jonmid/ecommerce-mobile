0938009dc6f60a6834da5929a54a3c5ae88a4508
Jhonatan Mideros <jmideros@robinfood.com>
2024-04-04 19:10:51 -0500

    first commit

---
+++ lib/modules/authentication/screens/login/login.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: const Center(
+        child: Text('Login'),
+      ),
+    );
+  }
+}
