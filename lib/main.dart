b6083c00b3eb9734680641b8f1fface19313d548
Jhonatan Mideros <jmideros@robinfood.com>
2024-05-02 15:55:58 -0500

    [add] config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:app_mobile/app.dart';
+import 'package:app_mobile/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  // Todo: Add widgets binding
-  // Todo: Init Local Storage
-  // Todo: Await native splash
-  // Todo: Initialize Firebase
-  // Todo: Initialize Authentication
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// Todo: Add widgets binding
+  /// Todo: Init Local Storage
+  /// Todo: Await native splash
+  /// Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  /// Todo: Initialize Authentication
 
   runApp(const App());
 }
